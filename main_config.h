70e98bbd8066a69cbf5b1f0a0a17ba45c8daec13
andreicosmin02 <andrei.achim.6669@gmail.com>
2025-01-20 23:04:31 +0200

    Added board configuration files for microcontroller

---
+++ main_config.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_CONFIG_H_
+#define MAIN_CONFIG_H_
+
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
+
+
+
+#endif /* MAIN_CONFIG_H_ */
---
+++ main_mcu_init.c
@@ -0,0 +1,19 @@
+#include "main_mcu_init.h"
+
+void port_init(void)
+{
+	PORTD = 0x00;
+	PORTB = 0x00;
+}
+
+void timer1_init(void)
+{
+	TCCR1A |= 1 << WGM12;	// CTC Mode
+	TCCR1B |= 1 << CS11;	// Prescaler 8
+}
+
+void init_devices(void)
+{
+	timer1_init();
+	port_init();
+}
