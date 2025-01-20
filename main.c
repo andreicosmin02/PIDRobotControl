c8b71caa4a6553d262f28b2c6ff01d32da9b98db
andreicosmin02 <andrei.achim.6669@gmail.com>
2025-01-20 22:57:57 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ProiectFinal.c
+ *
+ * Created: 1/20/2025 10:50:09 PM
+ * Author : andre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
